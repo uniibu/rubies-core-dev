b5ac015d20db9028ad803dced2e0a5d32c61f851
mammix2 <mammix2@hotmail.com>
2016-01-03 21:46:51 +0000

    Rubies pre-release testing

---
+++ README.md
@@ -0,0 +1,8 @@
+
+rubies core
+
+Use this repo for the cut down version of the wallet.
+
+The first ever x13 PoW / PoS coin.
+
+Visit http://rubies.eu for more details, or visit the launch thread over at https://bitcointalk.org/index.php?topic=601965.0
---
+++ src/checkblocks.h
@@ -0,0 +1,16 @@
+#ifndef CHECKBLOCKS_H
+#define CHECKBLOCKS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+static const uint256 hashGenesisBlock("00000099886cd33661edf127b6eaa5a46dd107ef4b7e0c6aea4a2d9beec95457");
+static const uint256 hashGenesisBlockTestNet("00000099886cd33661edf127b6eaa5a46dd107ef4b7e0c6aea4a2d9beec95457");
+
+// Chechpoints to ensure the correct chain usage during sync
+
+
+// Testnet Checkpoints
+
+
+#endif // CHECKBLOCKS_H
